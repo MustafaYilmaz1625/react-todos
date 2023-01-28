a9cd8aad82413c7d83a2c6b9c148a9db73439333
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-01-28 23:12:10 +0300

    add todo.jsx

---
+++ src/components/todo/Todo.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Form from "../form/Form";
+import List from "../list/List";
+import Button from "../button/Button";
+import Footer from "../footer/Footer";
+
+function Todo() {
+  const [todo, setTodo] = useState([]);
+  const [status, setStatus] = useState("all");
+
+  return (
+    <div>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <Form todo={todo} setTodo={setTodo} />
+        </header>
+        <List todo={todo} setTodo={setTodo} status={status} />
+        <Button todo={todo} setTodo={setTodo} setStatus={setStatus} />
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default Todo;
